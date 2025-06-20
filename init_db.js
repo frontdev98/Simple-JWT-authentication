90b9bcf1f2527cd3df96d32b281533b530272713
vsvirskiy <vsvirskiy98@gmail.com>
2025-06-20 09:03:36 +0300

    create model "role"

--- init_db.js
+++ init_db.js
@@ -3,14 +3,30 @@ const pool = require('./db');
 async function initDb() {
     try {
         await pool.query(`
+
+            /* Use new schema for the project */
             CREATE SCHEMA IF NOT EXISTS jwt_auth;
 
-            CREATE TABLE IF NOT EXISTS jwt_auth.persons (
-                id			SERIAL			PRIMARY KEY,
-                email		VARCHAR(255) 	NOT NULL,
-                password	VARCHAR(255)	NOT NULL,
-                created_at	TIMESTAMP WITH TIME ZONE DEFAULT NOW()
+            /* Set default schema to jwt_auth */
+            SET search_path TO 'jwt_auth';
+
+            CREATE TABLE IF NOT EXISTS roles (
+                id          SERIAL              PRIMARY KEY,
+                name        VARCHAR(255)        NOT NULL UNIQUE
             );
+
+            CREATE TABLE IF NOT EXISTS persons (
+                id			SERIAL			    PRIMARY KEY,
+                email		VARCHAR(255) 	    NOT NULL UNIQUE,
+                password	VARCHAR(255)	    NOT NULL,
+                created_at	TIMESTAMP WITH TIME ZONE DEFAULT NOW(),
+                role        INT                 NOT NULL,
+                FOREIGN KEY (role) REFERENCES roles (id)
+            );
+
+            /* Create two roles: "admin" and "user" */
+            INSERT INTO roles (name) VALUES ('admin');
+            INSERT INTO roles (name) VALUES ('user');
         `);
 
     } catch (e) {
--- models/user.js
+++ models/user.js
@@ -2,32 +2,23 @@ const pool = require('../db');
 
 class User {
     async getAll() {
-        return await pool.query(`SELECT * FROM jwt_auth.persons;`).rows;
+        const users = await pool.query(`SELECT * FROM jwt_auth.persons;`).rows;
+        return users;
     }
 
     async add(email, password) {
-        const user = await pool.query(
-            `INSERT INTO jwt_auth.persons VALUES ($1, $2) RETURNING *`, [email, password]
-        );
-
+        const user = await pool.query(`INSERT INTO jwt_auth.persons VALUES ($1, $2) RETURNING *`, [email, password]);
         return user.rows[0];
     }
 
     async update(id, data) {
-        const queryStr = `UPDATE jwt_auth.persons SET ` +
-                        Object.entries(data).map(([field, value]) => `${field} = ${value}`).join(', ') +
-                        `WHERE id = ${id} RETURNING *`;
-
-        console.log(queryStr);
-
-        const user = await this.pool.query(queryStr);
-
+        const dataSetStr = Object.entries(data).map(([field, value]) => `${field} = ${value}`).join(', ');  // build string with data
+        const user = await this.pool.query(`UPDATE jwt_auth.persons SET ${dataSetStr} WHERE id = ${id} RETURNING *;`);
         return user.rows[0];
     }
 
     async delete(id) {
         const user = await this.pool.query(`DELETE FROM jwt_auth.persons WHERE id = ${id}`);
-
         return user.rows[0];
     }
 }
