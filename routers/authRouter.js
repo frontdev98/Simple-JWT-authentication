f542be814f32993b196aec4d5f43d2d07b9aa2f3
Valentin Svirkiy <vsvirskiy98@gmail.com>
2025-06-21 11:38:59 +0300

    Add checking of HTTP method to /register, /login

--- routers/authRouter.js
+++ routers/authRouter.js
@@ -7,10 +7,17 @@ router.post('/register', [
     check('email', `Field email can't be empty!`).notEmpty().isEmail(),
     check('password', `Password must be minimum 8 and maximum 255 characters!`).notEmpty().isLength({min:8, max:255})
 ], controller.register);
+
 router.post('/login', [
     check('email', `Field email can't be empty!`).notEmpty().isEmail(),
     check('password', `Password must be minimum 8 and maximum 255 characters!`).isLength({min:8, max:255})
 ],controller.login);
-router.get('/user', controller.getUsers);
+
+// POST only for "/register", "/login"
+router.all(/register|login/, (req, res) => {
+    if (req.method != 'POST') {
+        return res.status(405).end();
+    }
+});
 
 module.exports = router;
