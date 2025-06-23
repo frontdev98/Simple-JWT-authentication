9a55f05a813e25b299eb875c7dde43d5da412d58
vsvirskiy <vsvirskiy98@gmail.com>
2025-06-23 09:17:07 +0300

    Remove log to file (nodemon conflict).

--- logger.js
+++ logger.js
@@ -4,20 +4,23 @@ const logger = winston.createLogger({
     format: winston.format.json(),
     defaultMeta: {service: 'JWT-Auth'},
     transports: [
-        new winston.transports.File({
-            filename: process.env.LOG_PATH
+        // new winston.transports.File({
+        //     filename: process.env.LOG_PATH
+        // })
+        new winston.transports.Console({
+            format: winston.format.simple()
         })
     ],
     exitOnError: false
 });
 
-if (process.env.NODE_ENV === 'dev') {
-    logger.add(
-        new winston.transports.Console({
-            format: winston.format.simple()
-        })
-    );
-}
+// if (process.env.NODE_ENV === 'dev') {
+//     logger.add(
+//         new winston.transports.Console({
+//             format: winston.format.simple()
+//         })
+//     );
+// }
 
 module.exports = {
     logger
